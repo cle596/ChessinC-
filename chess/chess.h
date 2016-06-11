e0ed3df0f286b24071feeb31fabbbd50bed02ae3
changhan <changhanlee@gmail.com>
2016-06-11 10:19:04 -0400

    chess.h header

--- chess/chess.h
+++ chess/chess.h
@@ -5,3 +5,29 @@
 
 std::vector<std::string>
 gen_moves(std::string);
+
+std::string 
+add_space_to_board(std::string);
+
+std::string
+move_to_pos(std::string);
+
+std::string
+pos_to_move(std::string);
+
+std::vector<std::string>
+translate_moves(std::vector<std::string>);
+
+std::string init_board =
+	"         \n"
+	"         \n"
+	" rnbqkbnr\n"
+	" pppppppp\n"
+	" ........\n"
+	" ........\n"
+	" ........\n"
+	" ........\n"
+	" PPPPPPPP\n"
+	" RNBQKBNR\n"
+	"         \n"
+	"         \n";
