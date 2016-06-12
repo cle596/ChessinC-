134a8a86ed7932e8fe4402c6c7f91c7015f05f46
changhan <changhanlee@gmail.com>
2016-06-12 11:45:01 -0400

    vector by reference

--- chess/gen.cpp
+++ chess/gen.cpp
@@ -7,30 +7,28 @@
 
 #include "gendefs.h"
 
-std::vector<std::string>
-pawn(std::vector<std::string> moves,std::string board,int x) {
+void
+pawn(std::vector<std::string>& moves,std::string board,int x) {
 	if (board.at(x + up) == '.') {
 		moves.push_back(std::to_string(x).append(std::to_string(x + up)));
 		if (board.at(x + 2 * up) == '.') {
 			moves.push_back(std::to_string(x).append(std::to_string(x + 2 * up)));
 		}
 	}
-	return moves;
 }
 
-std::vector<std::string>
-knight(std::vector<std::string> moves, std::string board, int x) {
+void
+knight(std::vector<std::string>& moves, std::string board, int x) {
 	for (int y = 0; y < 8; ++y) {
 		if (board.at(x + Nmoves[y]) == '.' ||
 			std::find(std::begin(foe), std::end(foe), board.at(x + Nmoves[y])) != std::end(foe)) {
 			moves.push_back(std::to_string(x).append(std::to_string(x + Nmoves[y])));
 		}
 	}
-	return moves;
 }
 
-std::vector<std::string>
-bishop(std::vector<std::string> moves, std::string board, int x) {
+void
+bishop(std::vector<std::string>& moves, std::string board, int x) {
 	for (int y = 0; y < 4; ++y) {
 		int z = 1;
 		while (board.at(x + z*Bmoves[y]) == '.' ||
@@ -45,11 +43,10 @@ bishop(std::vector<std::string> moves, std::string board, int x) {
 			z += 1;
 		}
 	}
-	return moves;
 }
 
-std::vector<std::string>
-rook(std::vector<std::string> moves, std::string board, int x) {
+void
+rook(std::vector<std::string>& moves, std::string board, int x) {
 	for (int y = 0; y < 4; ++y) {
 		int z = 1;
 		while (board.at(x + z*Rmoves[y]) == '.' ||
@@ -64,11 +61,10 @@ rook(std::vector<std::string> moves, std::string board, int x) {
 			z += 1;
 		}
 	}
-	return moves;
 }
 
-std::vector<std::string>
-queen(std::vector<std::string> moves, std::string board, int x) {
+void
+queen(std::vector<std::string>& moves, std::string board, int x) {
 	for (int y = 0; y < 8; ++y) {
 		int z = 1;
 		while (board.at(x + z*Kmoves[y]) == '.' ||
@@ -83,7 +79,19 @@ queen(std::vector<std::string> moves, std::string board, int x) {
 			z += 1;
 		}
 	}
-	return moves;
+}
+
+void
+king(std::vector<std::string>& moves, std::string board, int x) {
+	for (int y = 0; y < 8; ++y) {
+		if (board.at(x + Kmoves[y]) == '.') {
+			moves.push_back(std::to_string(x).append(std::to_string(x + Kmoves[y])));
+		}
+		else if (std::find(std::begin(foe), std::end(foe), board.at(x + Kmoves[y])) != std::end(foe)) {
+			moves.push_back(std::to_string(x).append(std::to_string(x + Kmoves[y])));
+			break;
+		}
+	}
 }
 
 std::vector<std::string>
@@ -91,22 +99,22 @@ gen_moves(std::string board) {
 	std::vector<std::string> moves;
 	for (size_t x = 0; x < board.length(); ++x) {
 		if (board.at(x) == 'P') {
-			moves = pawn(moves,board,x);
+			pawn(moves,board,x);
 		}
 		else if (board.at(x) == 'N') {
-			moves = knight(moves, board, x);
+			knight(moves, board, x);
 		}
 		else if (board.at(x) == 'B') {
-			moves = bishop(moves, board, x);
+			bishop(moves, board, x);
 		}
 		else if (board.at(x) == 'R') {
-			moves = rook(moves, board, x);
+			rook(moves, board, x);
 		}
 		else if (board.at(x) == 'Q') {
-			moves = queen(moves, board, x);
+			queen(moves, board, x);
 		}
 		else if (board.at(x) == 'K') {
-
+			king(moves, board, x);
 		}
 	}
 	return moves;
--- chess/gen.h
+++ chess/gen.h
@@ -1,19 +1,22 @@
 #pragma once
 
-std::vector<std::string>
-pawn(std::vector<std::string>, std::string, int);
+void
+pawn(std::vector<std::string>&, std::string, int);
 
-std::vector<std::string>
-knight(std::vector<std::string>, std::string, int);
+void
+knight(std::vector<std::string>&, std::string, int);
 
-std::vector<std::string>
-bishop(std::vector<std::string>, std::string, int);
+void
+bishop(std::vector<std::string>&, std::string, int);
 
-std::vector<std::string>
-rook(std::vector<std::string>, std::string, int);
+void
+rook(std::vector<std::string>&, std::string, int);
 
-std::vector<std::string>
-queen(std::vector<std::string>, std::string, int);
+void
+queen(std::vector<std::string>&, std::string, int);
+
+void
+king(std::vector<std::string>&, std::string, int);
 
 std::vector<std::string>
 gen_moves(std::string);
